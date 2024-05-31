e662cce105cc7725d5c7041ac1f6e51e9f354bf5
Luis Lucas <llnet@pop-os.lan>
2024-05-31 17:45:06 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    copyPublicDir: false,
+    lib:{
+      entry: resolve(__dirname, 'lib/main.js'),
+      formats: ['es', 'cjs'],
+      name: 'FloatingForm'
+    },
+    rollupOptions:{
+        external: ['vue'],
+        output: {
+            globals: {
+            vue: 'Vue'
+            }
+        }
+    }
+  }
 })
